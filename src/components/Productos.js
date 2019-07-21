1e70a58e451a29cba6aa78bdbd9c4145c03881b8
musiciancoder <45342398+musiciancoder@users.noreply.github.com>
2019-07-21 13:07:34 -0400

    Add files via upload

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import ProductoLista from './ProductoLista';
 
 
 
-function Productos() { /*productos desde App*/
+function Productos({productos}) { /*productos desde App*/
 
     return(
-
+<Fragment>
             <h1 className="text-center">Productos</h1>
+    <ul className="list-group mt-5">
+        {productos.map(producto=>(
+            <ProductoLista
+            key={producto.id}
+            producto={producto}
+            />
 
+        ))}
 
+    </ul>
+
+</Fragment>
     )
 
 }
