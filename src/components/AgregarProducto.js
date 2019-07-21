6451da52426c31e4bb778da9775b802ad1af29d9
musiciancoder <45342398+musiciancoder@users.noreply.github.com>
2019-07-21 14:40:53 -0400

    165
    165 final

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,9 +1,102 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function AgregarProducto() {
 
+
+    const [nombrePlatillo, guardarNombre]= useState(''); //lo que se escribe en input Nombre
+    const [precioPlatillo, guardarPrecio]= useState('');//lo que se escribe en input Precio
+    const [categoriaPlatillo, guardarCategoria]= useState('');//lo que se escribe en input categoria
+
+    const leerValorRadio = e => /*aca lee el radiobutton*/
+        guardarCategoria(e.target.value);
+
     return(
-    <h1 className="text-center">Agregar Nuevo Producto</h1>
+        <div className="col-md-8 mx-auto ">
+            <h1 className="text-center">Agregar Nuevo Producto</h1>
+
+            <form
+                className="mt-5"
+            >
+                <div className="form-group">
+                    <label>Nombre Platillo</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="nombre"
+                        placeholder="Nombre Platillo"
+                        onChange={e=> guardarNombre(e.target.value)}/* al comenzar a escribir en el input, el state debe cambiar en react tools*/
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Precio Platillo</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        name="precio"
+                        placeholder="Precio Platillo"
+                        onChange={e=> guardarPrecio(e.target.value)}
+                    />
+                </div>
+
+                <legend className="text-center">Categoría:</legend>
+                <div className="text-center">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="postre"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Postre
+                        </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="bebida"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Bebida
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="cortes"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Cortes
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="ensalada"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Ensalada
+                        </label>
+                    </div>
+                </div>
+
+                <input type="submit" className="font-weight-bold text-uppercase mt-5 btn btn-primary btn-block py-3" value="Agregar Producto" />
+            </form>
+        </div>
+
 
     )
 
