1100f41a88fd2b5bf8bc0c8fdfe095a4f775ad48
musiciancoder <45342398+musiciancoder@users.noreply.github.com>
2019-07-21 14:57:50 -0400

    166

---
+++ components/AgregarProducto.js
@@ -0,0 +1,124 @@
+import React, {useState} from 'react';
+import Error  from './Error';
+
+function AgregarProducto() {
+
+
+    const [nombrePlatillo, guardarNombre]= useState(''); //lo que se escribe en input Nombre
+    const [precioPlatillo, guardarPrecio]= useState('');//lo que se escribe en input Precio
+    const [categoria, guardarCategoria]= useState('');//lo que se escribe en input categoria
+    const [error, guardarError] = useState(false);
+
+    const leerValorRadio = e => { /*aca lee el radiobutton*/
+        guardarCategoria(e.target.value);
+    }
+
+    const  agregarProducto = e => {
+        e.preventDefault();
+
+        if(nombrePlatillo===''||precioPlatillo===''||categoria==='') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false); //volvemos al estado original
+
+        //Crear el nuevo producto
+
+    }
+
+    return(
+        <div className="col-md-8 mx-auto ">
+            <h1 className="text-center">Agregar Nuevo Producto</h1>
+
+            {(error)? <Error mensaje='Todos los campos son obligatorios'/>:null}
+            <form
+                className="mt-5"
+                onSubmit={agregarProducto}
+            >
+                <div className="form-group">
+                    <label>Nombre Platillo</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="nombre"
+                        placeholder="Nombre Platillo"
+                        onChange={e=> guardarNombre(e.target.value)}/* al comenzar a escribir en el input, el state debe cambiar en react tools*/
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Precio Platillo</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        name="precio"
+                        placeholder="Precio Platillo"
+                        onChange={e=> guardarPrecio(e.target.value)}
+                    />
+                </div>
+
+                <legend className="text-center">Categoría:</legend>
+                <div className="text-center">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="postre"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Postre
+                        </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="bebida"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Bebida
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="cortes"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Cortes
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="ensalada"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Ensalada
+                        </label>
+                    </div>
+                </div>
+
+                <input type="submit" className="font-weight-bold text-uppercase mt-5 btn btn-primary btn-block py-3" value="Agregar Producto" />
+            </form>
+        </div>
+
+
+    )
+
+}
+
+export default AgregarProducto;
---
+++ components/Producto.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Producto() {
+
+    return(
+
+        <h1 className="text-center">Datos del Producto</h1>
+
+
+    )
+}
+
+export default Producto;
