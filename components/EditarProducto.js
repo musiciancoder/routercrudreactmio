d0d46e3c894250fcf5781f54b82d6518900a2dd3
musiciancoder <45342398+musiciancoder@users.noreply.github.com>
2019-07-22 15:27:49 -0400

    171

---
+++ components/EditarProducto.js
@@ -0,0 +1,110 @@
+import React, {useState} from 'react';
+import Error from "./Error";
+
+function EditarProducto() {
+
+    const [error, guardarError] = useState(false);
+    const [categoria, guardarCategoria]= useState('');
+
+    const editarProducto = e=>{
+
+    }
+
+    const leerValorRadio = e => { /*aca lee el radiobutton*/
+        guardarCategoria(e.target.value);
+    }
+
+
+
+
+
+    return(
+
+        <div className="col-md-8 mx-auto ">
+            <h1 className="text-center">Editar Producto</h1>
+
+            {(error)? <Error mensaje='Todos los campos son obligatorios'/>:null}
+            <form
+                className="mt-5"
+                onSubmit={editarProducto}
+            >
+                <div className="form-group">
+                    <label>Nombre Platillo</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="nombre"
+                        placeholder="Nombre Platillo"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Precio Platillo</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        name="precio"
+                        placeholder="Precio Platillo"
+                    />
+                </div>
+
+                <legend className="text-center">Categoría:</legend>
+                <div className="text-center">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="postre"
+                            onChange={leerValorRadio}
+                        />
+                        <label className="form-check-label">
+                            Postre
+                        </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="bebida"
+                        />
+                        <label className="form-check-label">
+                            Bebida
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="cortes"
+                        />
+                        <label className="form-check-label">
+                            Cortes
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="ensalada"
+                        />
+                        <label className="form-check-label">
+                            Ensalada
+                        </label>
+                    </div>
+                </div>
+
+                <input type="submit" className="font-weight-bold text-uppercase mt-5 btn btn-primary btn-block py-3" value="Editar Producto" />
+            </form>
+        </div>
+
+
+)
+}
+
+export default EditarProducto;
